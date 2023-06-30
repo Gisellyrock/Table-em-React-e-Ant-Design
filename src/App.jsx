47544b751bf52dac6e231f0a1294bb45ddf3568f
GisellyRock <gisellydsr@gmail.com>
2023-06-30 09:14:34 -0300

    adding modals

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import './App.css';
-import { Button, Table } from 'antd';
+import { Button, Table, Modal } from 'antd';
 import { useState } from 'react';
 import { EditOutlined, DeleteOutlined } from '@ant-design/icons';
 
 function App() {
+  const [isEditing, setIsEditing] = useState(false);
   const [dataSource, setDataSource] = useState([
     {
       id: 1,
@@ -56,7 +57,21 @@ function App() {
       key: '5',
       title: 'Actions',
       render: (record) => {
-        return;
+        return (
+          <>
+            <EditOutlined
+              onClick={() => {
+                onEditStudent(record);
+              }}
+            />
+            <DeleteOutlined
+              onClick={() => {
+                onDeleteStudent(record);
+              }}
+              style={{ color: 'red', marginLeft: 12 }}
+            />
+          </>
+        );
       },
     },
   ];
@@ -73,10 +88,40 @@ function App() {
       return [...pre, newStudent];
     });
   };
+
+  const onDeleteStudent = (record) => {
+    Modal.confirm({
+      title: 'Are you sure, you want to delete this student record?',
+      okText: 'Yes',
+      okType: 'danger',
+      onOk: () => {
+        setDataSource((pre) => {
+          return pre.filter((student) => student.id !== record.id);
+        });
+      },
+    });
+    setDataSource((pre) => {
+      return pre.filter((student) => student.id !== record.id);
+    });
+  };
+
+  const onEditStudent = (record) => {
+    setIsEditing(true);
+  };
   return (
     <>
       <Button onClick={onAddStudent}> Add a New Student </Button>
       <Table columns={columns} dataSource={dataSource}></Table>
+      <Modal
+        title="Edit Student"
+        open={isEditing}
+        onCancel={() => {
+          setIsEditing(false);
+        }}
+        onOk={() => {
+          setIsEditing(false);
+        }}
+      ></Modal>
     </>
   );
 }
